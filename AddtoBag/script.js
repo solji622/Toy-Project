af970dff87c5ec7e7b1740cae1cf82c42e9a86e5
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-09-02 16:32:29 +0900

    Add files via upload

--- AddtoBag/script.js
+++ AddtoBag/script.js
@@ -1,12 +1,23 @@
 var mainImg = document.querySelector("#main")
-var itemImg = document.querySelectorAll("#item")
+var itemImg = document.querySelectorAll(".item")
+
+if (itemImg.length > 0) {
+    mainImg.setAttribute("src", itemImg[0].getAttribute("src"))
+    itemImg[0].classList.add('on');
+}
 
 
 for (var i = 0; i < itemImg.length; i++) {
-    itemImg[i].addEventListener("click", changeImg)
+    itemImg[i].addEventListener("click", changeImg);
 }
 
 function changeImg() {
     var items = this.getAttribute("src")
     mainImg.setAttribute("src", items)
+
+    itemImg.forEach(item => {
+        item.classList.remove('on')
+    })
+
+    this.classList.add('on')
 }
